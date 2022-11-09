c0f3cda3fe947a3f6c1ad17862dd27c468696a18
ThePhantomEye <thephantomeye.cubing@gmail.com>
2022-11-09 20:10:17 +0100

    -

--- main.py
+++ main.py
@@ -1,28 +1,17 @@
 import math
 from PIL import Image
 
+block_height = 3
+
+
 def encodeImage(image_path, text_path):
     image = Image.open(image_path)
     image = image.convert("1")
 
-    block_height = 3
-    """
-    while True:
-        try:
-            print("Enter the amount of lines to skip:")
-            block_height = int(input())
-            if 10 > block_height > 0:
-                break
-            else:
-                print("Please enter a value between 1 and 9")
-        except ValueError:
-            print("Please enter a valid integer")
-    """
     width = image.size[0]
     height = image.size[1]
     block_count = math.ceil(height / block_height)
 
-    colour = False
     cursor_color = False
     cursor_value = 0
 
@@ -34,34 +23,21 @@ def encodeImage(image_path, text_path):
 
     for block_y in range(block_height):
         for block_index in range(block_count):
-            for i in range(width):
+            x = 0
+            while x <= width:
                 y = block_index * block_height + block_y
                 if y >= height:
                     break
                 if x == 0 and y == 0:
                     pass
                 if x >= width:
-                    x = 0
-
-                # if x == 0 and block_index == 6:
-                #     print("now")
-
-                # print(image.getpixel((x, y)))
-                # print(image.getpixel((x, y)) / 255)
-
-                # print(x, height - y + 1)
+                    break
                 color = bool(image.getpixel((x, height - y - 1)) / 255)
-                # if colour != color:
-                #     cursor_value += 1
-                #
-                # colour = color
                 if color == cursor_color and cursor_value < ord(max_char) - ord(min_char):
                     cursor_value += 1
                     x += 1
                 else:
                     output += chr(ord(min_char) + cursor_value)
-                    if output == "APERTURE IMAGE FORMAT (c) 1985\n3\n~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ i!'!~ ~ ~ 7%!%\"%#\"!\"\"\"%\"\"\"\"!#!%\"\"~ ~ g%!\"\"\"!\"#\"!\"!\"!\"%%\"\"\"\"!\"\"%~ ~ l#\"!!##!!\"#\"!!$####!!#\"#\"#~ ~ ~":
-                        print("wrong")
                     cursor_color = not cursor_color
                     cursor_value = 0
 
@@ -73,50 +49,75 @@ def encodeImage(image_path, text_path):
     text_file.write(output)
     text_file.close()
 
-"""
-def decodeText(text_path, image_path):
+
+def decodeImage(text_path, image_path):
     text_file = open(text_path, "r")
     text = text_file.read()
     text_file.close()
-    header = text.split("\n")[0].split(";")
-    content = text.split("\n")[1]
-    width = int(header[0])
-    height = int(header[1])
-    block_height = int(header[2])
-    min_char = ord(header[3])
-    max_char = ord(header[4])
+    content = text.split("\n")[2]
+    width = 320
+    height = 200
+    block_height = int(text.split("\n")[1])
+    min_char = " "
+    max_char = "~"
     cursor_color = False
     pixel_index = 0
     image = Image.new("1", (width, height))
+
     for char in content:
-        for count in range(ord(char) - min_char + 1):
+        for count in range(ord(char) - ord(min_char) + 1):
             x = pixel_index % width
-#           y = int((pixel_index - x) / width)  # to fix
-            y = (pixel_index - x) // width
+            y = int((((pixel_index - x) / width) * block_height) % height)  # fix
             if pixel_index >= width * height:
                 break
- 
+
             image.putpixel((x, y), cursor_color * 255)
             pixel_index += 1
         cursor_color = not cursor_color
     image.save(image_path)
     image.close()
-"""
+
 
 def menu():
+    print("Select an option:\n  1) encode\n  2) decode\n  3) exit")
     while True:
         option = input()
-        if option.upper() == 'ENCODE':
-            encodeImage("image_original.png", "image_encoded.apf")
-            print("\033[31;1m\033[38:5:4mOK\033[0m")
-            break
-        elif option.upper() == 'DECODE':
-            decodeImage("image_encoded.apf", "image_decoded.png")
-            print("\033[31;1m\033[38:5:4mOK\033[0m")
-            break
-        elif option.upper() == 'EXIT':
-            exit
-        else:
-            print('Invalid choice. \n Please choose between: [encode], [decode], [exit]')
+        if option == '1':  # chosen the encode option
 
-menu()
+            print("Please the amount of lines to skip:")  # define block_height
+            while True:
+                try:
+                    block_height = int(input())
+                    if 10 > block_height > 0:
+                        break
+                    else:
+                        print("Please enter a value between 1 and 9")
+                except ValueError:
+                    print("Please enter a valid integer")
+
+            while True:
+                print("Enter the file name of the input image:")  # define input_file
+                input_file = input()
+                print("Enter the desired  name of the output file:")  # define output_file
+                output_file = input()
+                try:
+                    encodeImage(input_file, output_file)  # encode image
+                    print("\033[31;1m\033[38:5:4mOK\033[0m")
+                    break
+                except FileNotFoundError:
+                    print("could not find " + input_file + ". Please try again.")
+
+        elif option == '2':  # chosen the decode option
+            # decodeImage("image_encoded.apf", "image_decoded.png")
+            print("\033[31;1m\033[38:5:4mOK\033[0m")
+            break
+        elif option == '3':  # chosen the exit option
+            print("Terminated script.")
+            exit()
+        else:
+            print("\nInvalid choice. \nPlease choose between:\n  1) encode\n  2) decode\n  3) exit")
+
+
+# menu()
+encodeImage("image_original.png", "image_encoded.apf")
+decodeImage("image_encoded.apf", "image_decoded.png")
